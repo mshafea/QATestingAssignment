1dd2ac992d671802ff797f0f644c974bb9407f60
Mohamed Shafea <mohshafea@gmail.com>
2018-02-20 20:11:45 +0200

    update Constant.java by adding relative path for Path_TestData

--- src/main/java/utility/Constant.java
+++ src/main/java/utility/Constant.java
@@ -3,7 +3,7 @@ package utility;
 public class Constant {
 	public static final String HOME_URL = "http://computer-database.herokuapp.com/computers";
 	public static final String ADD_NEW_COMPUTER_URL = "http://computer-database.herokuapp.com/computers/new";
-	public static final String Path_TestData = "C://Users//mshafea//Desktop//Case Study//git//Regression_Suite//src//main//java//testdata//";
+	public static final String Path_TestData = ".\\src\\main\\java\\testdata\\";
 	public static final String File_TestData = "TestData.xlsx";
 	// Test Data Sheet Columns
 	public static final int Col_TestCaseName = 0;
